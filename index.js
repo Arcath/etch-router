6a15e6bef1757c208d13b86e2685b543278b1562
Adam Laycock <adam@arcath.net>
2017-05-23 12:39:39 +0100

    wrapper component, closes #8

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const Link = require('./lib/components/link')
 const MissingRoute = require('./lib/components/missing-route')
 const Router = require('./lib/components/router')
 const Route = require('./lib/components/route')
+const Wrapper = require('./lib/components/wrapper')
 
 
 module.exports = {
-  Link, MissingRoute, Router, Route
+  Link, MissingRoute, Router, Route, Wrapper
 }
